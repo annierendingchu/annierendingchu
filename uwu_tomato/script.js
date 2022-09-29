0f73888b8a174fdb49cfa6ce744f88aa185f98bf
annierendingchu <81434326+annierendingchu@users.noreply.github.com>
2022-09-29 01:44:11 -0400

    basically done

--- uwu_tomato/script.js
+++ uwu_tomato/script.js
@@ -7,10 +7,45 @@ $(document).ready(function(){
         alert("What an adorable tomato.");
     });
 
+    $(".freedom").click(function(){
+        alert("Enjoy it while it lasts.");
+    });
+
+    $("#free").click(function(){
+        alert("Congrats!");
+    });
+
     $(".panic").click(function(){
         alert("To free you.");
     });
 
+    $("#feel").click(function(){
+        alert("Don't worry. Life is but a dream...");
+    });
+
+    $(".fainting").click(function(){
+        alert("You won't be sad if you feel nothing.");
+    });
+
+    $("#sticky4").click(function(){
+        alert("Sometimes I dream of being a human. --UWU");
+    });
+
+    $("#sticky3").click(function(){
+        alert("I once fell in love with a strawberry, but she was too insecured about her acnes to stay with me. --UWU");
+    });
+
+    $("#sticky1").click(function(){
+        alert("blingbling");
+    });
+
+    $("#sticky2").click(function(){
+        alert("sparkspark");
+    });
+
+    $("#sticky6").click(function(){
+        alert("See you soon.");
+    });
 
     $(window).mousemove(function(e) {
         let cursor = $(".cursor");
@@ -20,11 +55,9 @@ $(document).ready(function(){
         });
     });
 
-    $(window).mouseleave(function(e) {
-        window.location.replace("http:");
 
-        });
-    });
+    
+});
     //$("#hi").click(function(){
         //$(".img").attr('src','panic_right_Tomato.png')
       //  $("#hi").text("Wait, what are you doing?")
